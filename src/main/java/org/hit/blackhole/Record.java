0b1c42c5190cb8df65e34069b06b955174a38be1
xiongxoy <xiong.xoy@gmail.com>
2013-10-25 10:18:59 +0800

    Add Record checktime

--- src/main/java/org/hit/blackhole/Record.java
+++ src/main/java/org/hit/blackhole/Record.java
@@ -2,7 +2,6 @@ package org.hit.blackhole;
 
 import java.util.List;
 
-import com.google.common.base.Splitter;
 
 public class Record {
 	public static final int DT_S_TIME = 6-1;
@@ -17,18 +16,32 @@ public class Record {
 	public static final int INT_END_LAC = 21-1;
 	public static final int BI_KPI_FLAG = 51-1;
 	
+	public static final String T_START = "2012-03-02 08:00:00.000";
+	public static final String T_END = "2012-03-02 09:00:00.000";
+	
 	private List<String> items;
 
 	/**
-	 * XXX
 	 * @param s
 	 */
 	public Record( String s ) {
-		items = Splitter.on(',').splitToList(s);
+		items = StringUtil.split(s, ',');
 	}
 	
-	public String getItem (int i) {
+	public String getItem(int i) {
 		return items.get(i);
+	}
+	
+	public boolean isValid() {
+		return checkTime();
+	}
+	boolean checkTime() {
+		String start = this.getItem(DT_S_TIME);
+		if (start.compareTo(Record.T_START) >= 0 
+				&& start.compareTo(Record.T_END) < 0) {
+			return true;
+		}
+		return false;
 	}
 
 }
